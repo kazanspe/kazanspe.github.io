2bcfaedb7accfa5e8a0e4217fbc8f7e686fb69d3
SPE КНИТУ <speknrtu@gmail.com>
2017-07-08 11:19:55 +0300

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/contact@izaakbeekman.com",
+                url: "//formspree.io/kabayomi007@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
